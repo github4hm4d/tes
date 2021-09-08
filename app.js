4eaa0963d961c112128fc009f5cc7189eb9e4b4c
github4hm4d <90288606+github4hm4d@users.noreply.github.com>
2021-09-08 14:51:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,128 +1,24 @@
-const server = require('ws').Server;
-const port = process.env.PORT || 1953;
-const s = new server({ port: port});
-let counter = 0;
-let counteronline = 1;
-console.log("start");
-s.on('connection', function(ws){
-    counter++;
-    ws.on('message', function(message){
-        message = JSON.parse(message);
+const WebSocket = require('ws');
 
-        if (message.type == "sesion"){
-            s.clients.forEach(function e(client) {
-                if (client != ws) {
-                if (client.sesion == message.sesion) {
-                    counteronline = counteronline + 1;
-                }}
+const ws = new WebSocket.Server({ port: 1953 });
 
-            });
-
-            ws.sesion = message.sesion;
-            ws.nama = message.nama
-            return;
-        }else{
-            if (message.type == "statususer") {
-                console.log(counter + ' : '+ message.status +" : "+ message.nama);
-                s.clients.forEach(function e(client) {
-                        if (client.sesion == ws.sesion) {
-                            client.send(JSON.stringify({
-                                type: "statususer",
-                                status: "online",
-                                nama: ws.nama,
-                                counter: counteronline,
-                            }));
-                        }
-
-                    if (client.sesion == "498E432e605J210f844v817v609x577j219a446I343n339k617Z586S243V770R610m303n252L659m253U415V577v828U77O") {
-                        nama = ws.nama,
-                            client.send(JSON.stringify({
-                                type: "monitoring",
-                                counter: counter,
-                                nama: nama,
-                            }));
-                    }
+console.log("Server Started");
 
 
-                });
-        // when open
-            }
-            if (message.type == "message") {
-                console.log(message.nama+' : ' + message.message);
-            }
-        }
+ws.on('connection', (wss) => {
+
+  console.log("A new Client Connected")
+
+  wss.send('Welcome to the Server!');
 
 
+  wss.on('message', (message) => {
+
+    console.log(`Server Received: ${message}`);
 
 
-        s.clients.forEach(function e(client) {
-            if (client != ws) {
-            if (client.sesion == ws.sesion){
+    wss.send('Got your Message: ' + message);
 
-                if (message.type == "message") {
-                    client.send(JSON.stringify({
-                        type : "pesan",
-                        message: message.message,
-                        nama: message.nama,
-                        // sesion : ws.sesion,
-                        // temporary: message.temporary,
-                        // client: client.sesion,
-                        // ws: ws.sesion
-                    }));
-                }
-            }
-            }
-
-
-        });
-
-
-    });
-
-
-
-
-    ws.on("close", function(){
-        counter--;
-        console.log(ws.nama +" disconected: " + counter);
-        s.clients.forEach(function e(client) {
-            if (client != ws) {
-                if (client.sesion == ws.sesion){
-                    counteronline = counteronline - 1;
-                    client.send(JSON.stringify({
-                        type: "statususer",
-                        status: "offline",
-                        nama: ws.nama,
-                        counter: counteronline,
-                    }));
-                }
-            }
-
-            if (client.sesion == "498E432e605J210f844v817v609x577j219a446I343n339k617Z586S243V770R610m303n252L659m253U415V577v828U77O") {
-                nama = ws.nama,
-                    client.send(JSON.stringify({
-                        type: "monitoring",
-                        counter: counter,
-                        nama: nama,
-                    }));
-            }
-
-
-        });
-        // when close
-    });
-
-
-    console.log("1 koneksi baru: " + counter );
-
-
-    // setInterval(function () {
-    //     s.clients.forEach(function e(client) {
-    //         client.send(JSON.stringify({
-    //             type: "on",
-    //         }));
-    //     });
-    // }, 5000);
-
+  });
 
 });
