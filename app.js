b68dc10b84d29fe2a69c5345347d2e5b2edb788d
github4hm4d <90288606+github4hm4d@users.noreply.github.com>
2021-09-10 02:42:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-const WebSocket = require('wss');
-
-const ws = new WebSocket.Server({ port: 1953 });
-
-console.log("Server Started");
-
-
-ws.on('connection', (wss) => {
-
-  console.log("A new Client Connected")
-
-  wss.send('Welcome to the Server!');
-
-
-  wss.on('message', (message) => {
-
-    console.log(`Server Received: ${message}`);
-
-
-    wss.send('Got your Message: ' + message);
-
-  });
-
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+        response = "wellcome;
+    res.end(response);
 });
+server.listen();
